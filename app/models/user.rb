882790172c83290d4f723382e0798ac192ec5664
chikara-dot <victorycastlepower0209@gmail.com>
2021-05-03 12:09:48 +0000

    Finish4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,29 @@ class User < ApplicationRecord
  has_many :books, dependent: :destroy
  has_many :favorites, dependent: :destroy
  has_many :book_comments, dependent: :destroy
+
+ has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+ has_many :following_user, through: :active_relationships, source: :followed
+
+ has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+ has_many :follower_user, through: :passive_relationships, source: :follower
+
+
+ def follow(user_id)
+   active_relationships.create(followed_id: user_id)
+ end
+
+
+ def unfollow(user_id)
+   active_relationships.find_by(followed_id: user_id).destroy
+ end
+
+ def following?(user)
+   following_user.include?(user)
+ end
+
+
+
  attachment :profile_image
 
   validates :introduction, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,20 @@ Rails.application.routes.draw do
 
   get 'home/about' => 'homes#about'
   resources :users, only: [:index,:show,:edit,:create,:new,:update]
+
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
+
+
+  resources :relationship, onlu: [:create, :destroy]
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+
+  get 'follows' => 'relationships#follows'
+  get 'followers' => 'relationships#followers'
+
 
 end
